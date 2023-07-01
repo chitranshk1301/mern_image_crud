4c68fef88c5fd6935d14005d0aa5ff4c5c8da96e
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-07-02 00:18:38 +0530

    rm redux

--- client/src/pages/createPost.js
+++ client/src/pages/createPost.js
@@ -1,36 +1,49 @@
 import React from 'react'
 import { TextField, Button } from '@mui/material';
 import { useState } from 'react';
-import { connect } from 'react-redux';
-import { addImage } from '../redux/actions';
 
-const CreatePost = (error, addImage) => {
+const url = 'http://localhost:3000';
+
+const CreatePost = () => {
   const [fileName, setFileName] = useState('');
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
   const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    setSelectedImage(event.target.files[0]);
-    setFileName(file ? file.name : '');
+    setSelectedFile(event.target.files[0]);
+    setFileName(selectedFile ? selectedFile.name : '');
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+    formData.append('title', title);
+    formData.append('description', description);
 
-    if (selectedImage) {
-      addImage(selectedImage);
-    } 
+    const res = await fetch(`${url}/api/post`, {
+      method: 'POST',
+      body: formData,
+    },
+    { headers: { "Authorization": `Bearer ${localStorage.getItem("token")}` } } 
+    );
+    const data = await res.json();
+    console.log(data);
+    
+    if (data.success) {
+      window.location.href = '/home';
+    }
   }
 
   return (
     <div className='flex items-center justify-center'>
-      <form className='flex flex-col gap-5 md:h-2/6 md:w-1/4 items-center justify-center mt-24 py-10 border'>
+      <form className='flex flex-col gap-5 md:h-2/6 md:w-1/4 items-center justify-center mt-24 py-10 border' onSubmit={handleSubmit}>
         <div className="extraOutline p-4 bg-white min-w-[320px] bg-whtie m-auto rounded-lg">
           <div className="file_upload p-5 relative border-4 border-dotted border-gray-300 rounded-lg">
             <svg className="text-indigo-500 w-24 mx-auto mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" /></svg>
             <div className="input_field flex flex-col w-max mx-auto text-center">
               <label>
-                <input className="text-sm cursor-pointer w-36 hidden" type="file" accept="image/*" onChange={handleFileChange} multiple />
+                <input className="text-sm cursor-pointer w-36 hidden" type="file" accept="image/*" onChange={handleFileChange} />
                 <div className="text bg-indigo-600 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-indigo-500">Select</div>
               </label>
               
@@ -43,30 +56,19 @@ const CreatePost = (error, addImage) => {
 
         <TextField
           label="Title"
-        // value={username}
-        // onChange={(e) => setUsername(e.target.value)}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
 
         <TextField
           label="Description"
-        // value={username}
-        // onChange={(e) => setUsername(e.target.value)}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
 
         <Button variant="contained" size='large' onClick={handleSubmit}>Submit</Button>
-        {error && <p>{error}</p>}
       </form>
     </div>
   )
 }
-
-const mapStateToProps = (state) => ({
-  imageUrl: state.image.imageUrl,
-  error: state.image.error,
-});
-
-const mapDispatchToProps = {
-  addImage,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(CreatePost)
+export default CreatePost
