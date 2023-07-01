6a0606395d0ae24e01e08a2381f3cbfc59956275
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-07-01 18:50:10 +0530

    reducers setup

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -53,3 +53,4 @@ export const getImageById = (id) => {
 }
 
 
+
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import imageReducer from './reducers';
+import imageReducer from './imageReducer';
+import authReducer from './authReducers';
 
 const rootReducer = combineReducers({
   image: imageReducer,
+  auth: authReducer,
 });
 
 const store = configureStore(rootReducer, applyMiddleware(thunk));
